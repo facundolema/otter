d5365fd15b512f8390d2aa47f1ff247bdba38732
facundolema <facurlema@gmail.com>
2023-05-15 12:39:07 -0300

    populated about.md and algebra

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,9 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 // Reverse the sidebar items ordering (including nested category items)
 function reverseSidebarItems(items) {
   // Reverse items at current level
@@ -67,6 +70,8 @@ const config = {
       ({
         docs: {
           sidebarItemsGenerator: generator,
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -83,7 +88,6 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        
       }),
     ],
   ],
@@ -164,6 +168,15 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    stylesheets: [
+      {
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+        type: 'text/css',
+        integrity:
+          'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+        crossorigin: 'anonymous',
+      },
+    ],
 };
 
 module.exports = config;
